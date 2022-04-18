a8cf02182ae40748d13577580cf43ac84a3893fb
Juliano Resende <juliano.resende@gmail.com>
2022-04-17 23:02:42 -0300

    fixed issues about rotation log and validate file extensions

--- transfer-vod.py
+++ transfer-vod.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-
 import os
 import subprocess
 import logging
-import argparse
-import gzip
+from logging.handlers import TimedRotatingFileHandler
 import sqlite3
 from datetime import datetime, timedelta
 
@@ -16,13 +14,22 @@ CDN = "http://parc-media01-corpstreaming-tna-mia.terra.com/test/progressive/"
 
 # Log parameters
 TIMESTAMP = datetime.today().strftime("%d-%m-%Y")
-TRANSFER_LOG = "transfer_" + TIMESTAMP + ".log"
-logging.basicConfig(
-    filename=TRANSFER_LOG,
-    filemode="a",
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - [%(levelname)s] - %(message)s",
+TRANSFER_LOG = "transfer.log"
+
+# Log object
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Log Formatter
+formatter = logging.Formatter("%(asctime)s - %(name)s - [%(levelname)s] - %(message)s")
+
+# Log Handler
+handler = TimedRotatingFileHandler(
+    TRANSFER_LOG, when="midnight", backupCount=7, interval=1, encoding="utf-8"
 )
+handler.suffix = "_%d-%m-%Y"
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Allowed extensions file
 ALLOWED_EXTENSIONS = [".mp4", ".mov", ".wmv", ".avi", ".mkv", ".m4a", ".m4v"]
@@ -32,37 +39,6 @@ DB = sqlite3.connect("transfe-vod.db")
 
 
 def main():
-    parser = argparse.ArgumentParser(description="======= Transfer VOD - v0.2 =======")
-
-    """ Option: --download """
-    parser.add_argument(
-        "-d",
-        "--download",
-        action="store_true",
-        help="download file from remote location",
-    )
-
-    """ Option: --rotate-log """
-    parser.add_argument(
-        "-r",
-        "--rotate-log",
-        action="store_true",
-        help="compress, rotate and remove old logfiles.",
-    )
-
-    """ Get all args """
-    args = parser.parse_args()
-
-    if args.download:
-        download()
-    elif args.rotate_log:
-        rotate_logs()
-
-
-def download():
-
-    """Initialize Database"""
-    init_db()
 
     """Get list of file MOC NFS share"""
     for filename in os.listdir(INPUT_VOD):
@@ -72,16 +48,16 @@ def download():
             output_file_size = os.stat(os.path.join(OUTPUT_VOD, filename)).st_size
             """ Compare the file size to detect with the download still in progress """
             if input_file_size == output_file_size:
-                logging.info(filename + " are ready to transcode")
-                logging.info(filename + " will be removed from " + INPUT_VOD)
+                logger.info(filename + " are ready to transcode")
+                logger.info(filename + " will be removed from " + INPUT_VOD)
                 os.remove(os.path.join(INPUT_VOD, filename))
-                logging.info(filename + " has removed from " + INPUT_VOD)
+                logger.info(filename + " has removed from " + INPUT_VOD)
             else:
-                logging.info(filename + " still downloading...")
+                logger.info(filename + " still downloading...")
         else:
             filename_extension = os.path.splitext(filename)[1].lower()
             if filename_extension in ALLOWED_EXTENSIONS:
-                logging.info(filename + " wget started download")
+                logger.info(filename + " wget started download")
                 subprocess.Popen(
                     [
                         WGET,
@@ -95,7 +71,7 @@ def download():
                     close_fds=True,
                 )
             elif filename_extension in [".1", ".2", ".3"]:
-                logging.warning(
+                logger.warning(
                     filename
                     + " bad filename formatation, will be process at next cronjob."
                 )
@@ -104,48 +80,7 @@ def download():
                     os.path.join(INPUT_VOD, os.path.splitext(filename)[0]),
                 )
             else:
-                logging.error(filename + " extension its not supported")
-
-
-def rotate_logs():
-    yesterday = datetime.today() - timedelta(days=1)
-    yesterday = yesterday.strftime("%d-%m-%Y")
-
-    """ Get the yesterday log """
-    yesterday_log = "transfer_" + yesterday + ".log"
-    if os.path.isfile(yesterday_log):
-        log = open(yesterday_log, "rb")
-        logging.info(yesterday_log + " compress the yesterday log")
-        compress = gzip.open(yesterday_log + ".gz", "wb")
-        compress.writelines(log)
-        compress.close()
-        log.close()
-        logging.info(yesterday_log + " remove uncompress log")
-        os.remove(yesterday_log)
-
-
-def init_db():
-    cursor = DB.cursor()
-
-    # Create database if not exist
-    logging.info('Create database and job table')
-    cursor.execute(
-        """ CREATE TABLE IF NOT EXISTS jobs 
-                (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                    filename TEXT NOT NULL,  
-                    upload DATETIME, 
-                    download DATETIME, 
-                    transcode DATETIME, 
-                    finished DATETIME
-                )"""
-    )
-
-    # Commit changes
-    DB.commit()
-
-    # Close Databases
-    cursor.close()
+                logger.error(filename + " extension its not supported")
 
 
 if __name__ == "__main__":
